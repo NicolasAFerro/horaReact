cefe05473f76b9f3a9c13fce11bb794a41673ef1
NicolasAFerro <nicolasalexandrinoferro@gmail.com>
2025-04-27 21:53:37 -0300

    quase tudo

--- Avancando/avanc/src/components/ListRender.jsx
+++ Avancando/avanc/src/components/ListRender.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { useState } from "react";
 function ListRender() {
   const [list, setList] = useState(["Nicolas", "Gabi", "maria"]);
+  const [users, setUsers] = useState([
+    { id: 1, name: "Nicolas", age: 26 },
+    { id: 2, name: "Gabi", age: 27 },
+    { id: 3, name: "Maria", age: 28 },
+  ]);
   return (
     <div>
       <h3>ListRender</h3>
@@ -13,6 +18,14 @@ function ListRender() {
           <li>{item}</li>
         ))}
       </ul>
+      <div>
+        <p>UserList</p>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.age}
+          </li>
+        ))}
+      </div>
     </div>
   );
 }
