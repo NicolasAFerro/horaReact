f9f8177cbab44eb00f25e486bb322e82691458da
Nicolas Ferro <nicolasalexandrinoferro@gmail.com>
2025-05-02 16:22:09 -0300

    8

---
+++ 8_FormMultiStep/src/App.jsx
@@ -0,0 +1,34 @@
+import { GrFormPrevious,GrFormNext  } from 'react-icons/gr'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='app'> 
+      <div className='header'>
+        <h2>Deixe sua Avaliação</h2>
+        <p>ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto</p>
+      </div>
+      <div className="div form-container">
+        <p>etapas</p> 
+        <form action="">
+          <div className="inputs-container"></div>
+
+          <div className="actions">
+            <button type='button'>
+              <span>Voltar</span> 
+              <GrFormPrevious />
+            </button> 
+            <button type='submit'>
+              <span>Avançar</span> 
+              <GrFormNext  /> 
+            </button>
+          </div>  
+        </form> 
+
+      </div>    
+    </div>
+  )
+}
+
+export default App
